1fc4a62f4e7d56488daf4dc1a7072d96e9b208ce
Soklayy <soklayiter34@gmail.com>
2024-06-25 01:38:36 +0700

    first commit

--- app/(admin)/products/add-product/page.tsx
+++ app/(admin)/products/add-product/page.tsx
@@ -1,14 +1,9 @@
 import { getBrand } from "@/actions/brand";
 import { getCategory } from "@/actions/category";
 import New from "./New";
-import { Category } from "@/models/model";
-import { Dispatch, SetStateAction } from "react";
 
-interface Props {
-  refreshData: Dispatch<SetStateAction<Category[]>>
-}
 
-const ProductManagement = async ({ refreshData }: Props) => {
+const ProductManagement = async () => {
 
   const category = await getCategory()
   const brand = await getBrand()
